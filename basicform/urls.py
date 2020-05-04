32364329a12ffd3c2b08a12712b72bf3554dedb0
Tejpratp Singh <53250921+tejpratap545@users.noreply.github.com>
2020-05-04 16:26:03 +0530

    update urls.py

--- basicform/urls.py
+++ basicform/urls.py
@@ -19,4 +19,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
